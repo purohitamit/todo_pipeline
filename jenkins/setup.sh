aab0cedd90f4ada2f5e650619410e4751e067d99
Ubuntu <azureuser@myVM.5qh0wf5nsbruhlksmk54necr3c.zx.internal.cloudapp.net>
2021-11-16 18:04:47 +0000

    adding stages to the project

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-mkdir newdir3
-cd newdir3
-touch newfile
+#install apt dependencies
+
+sudo apt update
+sudo apt install python3-venv python3-pip python3 -y
+
+# create cirtual environment
+
+python3 -m venv venv
+
+# activate virtual env.
+
+source venv/bin/activate
+
+# install pip requirements
+
+pip3 install -r requirements.txt
+
